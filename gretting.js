0a30568902d3c70245292597c30970378cbcef94
sungkuk5420 <sungkuk5420@gmail.com>
2019-01-22 12:49:54 +0900

    사용자 이름 물어봐서 로컬 스토리지 저장.

---
+++ gretting.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+
+const init = () => {
+    loadName();
+};
+
+const saveName = (text) =>{
+    localStorage.setItem(USER_LS, text);
+}
+
+const paintGreeting = (text) => {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+};
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const userName = input.value;
+    paintGreeting(userName);
+    saveName(userName);
+}
+
+const askForName = () => {
+    form.classList.add(SHOWING_CN);
+    greeting.classList.remove(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit);
+};
+const loadName = (name) => {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    }else{
+        paintGreeting(currentUser);
+    }
+};
+init();
