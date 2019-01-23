19b60c7cc67577dd9e0b54ce2266fff1535bd305
sungkuk5420 <sungkuk5420@gmail.com>
2019-01-23 22:26:37 +0900

    , 초는 출력 안하도록 수정

--- clock.js
+++ clock.js
@@ -10,10 +10,9 @@ const getTime = () => {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
 
     clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:
-    ${minutes < 10 ? `0${minutes}` : minutes}:
-    ${seconds < 10 ? `0${seconds}` : seconds}`;
+    ${minutes < 10 ? `0${minutes}` : minutes}`;
 };
+
 init();
